cd2c9a4d777ec210b951d72f2cc9db2682b74e66
Kolja Dummann <k.dummann@gmail.com>
2011-04-28 16:05:12 +0200

    correct namespace

--- Messaging/IMessageHandler.cs
+++ Messaging/IMessageHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Messaging
+namespace SMS
 {
     public interface IMessageHandler<T>
     {
--- Messaging/Messenger.cs
+++ Messaging/Messenger.cs
@@ -20,7 +20,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Messaging
+namespace SMS
 {
 
     public static class Messenger
