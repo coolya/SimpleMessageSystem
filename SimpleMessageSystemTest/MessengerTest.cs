c9ebe1f7e38438faf724874212eccb852262666d
unknown <kdummann@.schulz-elektrotechnik.de>
2011-05-04 13:03:33 +0200

    rename unit test

---
+++ SimpleMessageSystemTest/MessengerTest.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright (C) 2011 Kolja Dummann <k.dummann@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SMS;
+
+
+namespace SimpleMessageSystemTest
+{
+    [TestClass]
+    public class MessengerTest
+    {
+        [TestMethod]
+        public void SimpleMessage()
+        {
+            Messenger.Subscribe<string>(TestStringHandler);
+            Messenger.GetMessage<string>().AddContent("Test Message").Publish();
+        }
+
+
+        private void TestStringHandler(Message<string> msg)
+        {
+            Assert.AreEqual("Test Message", msg.Content);
+        }
+    }
+}
